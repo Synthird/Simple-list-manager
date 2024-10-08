f5156c93a216466b90f6aac26ea387ec80277607
Synthird <105394020+Synthird@users.noreply.github.com>
2024-10-07 21:39:43 -0700

    Used pep8 extension formatter to format code

--- src/main.py
+++ src/main.py
@@ -5,43 +5,45 @@ listOfValues = [
     "B",
     "g",
     "WOW!"
-    ]
+]
+
 
 def findInList(value):
     if value in listOfValues:
         return True
-    
+
+
 def printResult(printValue):
     print(f">>>> {printValue} <<<<")
+
 
 print("What do you want to do with this list of values?")
 
 while True:
     print(listOfValues)
     print("--- INDIVIDUAL VALUES ---")
-    print("1 = Add something to the list\n2 = Remove something from the list\n3 = Find something in"
-        " the list\n4 = Get the index of a value")
+    print("1 = Add something to the list\n2 = Remove something from the list\n3 = Find something in the list\n4 = Get the index of a value")
     print("--- LIST ORDERING ---")
     print("5 = Organize the list\n6 = Reverse the order")
     print("--- OTHER ---")
-    print("7 = Count how many values there are\n8 = Clear the entire list\nAny other integer = Exit"
-        "this program")
-    
+    print("7 = Count how many values there are\n8 = Clear the entire list\nAny other integer = Exit this program")
+
     choice = int(input("I want to: "))
 
     if choice == 1:
-        addition = input("Add: ")
+        addition = input("I want to add: ")
 
         listOfValues.append(addition)
         printResult(f"{addition} has been added to the list!")
     elif choice == 2:
-        removing = input("Remove: ")
+        removing = input("I want to remove: ")
 
         if findInList(removing):
             listOfValues.remove(removing)
             printResult(f"{removing} has been removed from the list!")
         else:
-            printResult(f"Cannot remove {removing} since it it's not in the list...")
+            printResult(f"Cannot remove {
+                        removing} since it it's not in the list...")
     elif choice == 3:
         finding = input("I want to find: ")
 
@@ -53,9 +55,11 @@ while True:
         findValue = input("Find the index of ")
 
         if findInList(findValue):
-            printResult(f"The index of {findValue} is {str(listOfValues.index(findValue))}")
+            printResult(f"The index of {findValue} is {
+                        str(listOfValues.index(findValue))}")
         else:
-            printResult(f"Cannot find the value of {findValue} since it's not in the list")
+            printResult(f"Cannot find the value of {
+                        findValue} since it's not in the list")
     elif choice == 5:
         printResult("The list is now organized!")
         listOfValues.sort()
@@ -64,7 +68,8 @@ while True:
         listOfValues.reverse()
     elif choice == 7:
         listLength = len(listOfValues)
-        printResult(f"There are a total of {str(listLength)} values in the list")
+        printResult(f"There are a total of {
+                    str(listLength)} values in the list")
     elif choice == 8:
         listOfValues.clear()
         printResult("The entire list is cleared!")
